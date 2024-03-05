427a7c0b19cda067ec736f37d9c86815bbec43bf
Candyrizz <129657681+Candyris@users.noreply.github.com>
2024-03-05 19:57:23 +0530

    Update main.cpp
    Only made main structure

--- #1-Bookshop Management/WithoutDatabase/main.cpp
+++ #1-Bookshop Management/WithoutDatabase/main.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
+#define FILE_PATH Book_libray.info
+
+typedef unsigned int UINT;
+typedef std::string STR;
+
+
+class Book{
+public:
+    UINT bookID;
+    STR title;
+    STR author;
+    STR genre;
+    UINT quantity;
+};
+
+class BookShop{
+public:
+    // constructor and destructor functions
+    BookShop();
+    ~BookShop();
+
+    //Book operation function
+    void addBook();
+    void modifyBook();
+
+    // Loading and Saving file from memory
+    void loadDataFromFile();
+    void saveDataToFile();
+    
+    // printing data
+    void displayBOOKs();
+private:
+    std::vector<Book> books;
+};
+
+
+int main()
+{
+
+    return 0;
+}
